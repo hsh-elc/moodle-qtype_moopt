907f4bed3da1434e67d340490b494fb6576d665a
Paul Reiser <apaulreiser@gmail.com>
2021-10-04 18:20:19 +0200

    bump version

--- version.php
+++ version.php
@@ -25,6 +25,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_moopt';
 $plugin->release = '0.1.0';
-$plugin->version = 2021092700;
+$plugin->version = 2021100400;
 $plugin->requires = 2018120300;
 $plugin->maturity = MATURITY_ALPHA;
