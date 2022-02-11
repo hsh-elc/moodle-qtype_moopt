e2b8afb54ca02583082b8463cbd38a8ed7a7fdcf
Paul Reiser <apaulreiser@gmail.com>
2022-02-11 13:10:19 +0100

    Fix file link to embedded text files in proforma responses

--- classes/output/separate_feedback_text_renderer.php
+++ classes/output/separate_feedback_text_renderer.php
@@ -171,16 +171,16 @@ class separate_feedback_text_renderer
                         $content .= '<p>' . get_string('files', 'qtype_moopt') . ':<br/><ul>';
                         foreach ($files['embeddedFiles'] as $file) {
                             $pathinfo = pathinfo($this->fileinfos['filepath'] . $file['id'] . '/' . $file['filename']);
-                            $url = \moodle_url::make_pluginfile_url($this->fileinfos['contextid'], COMPONENT_NAME,
-                                PROFORMA_RESPONSE_FILE_AREA_EMBEDDED,
-                                $this->fileinfos['itemid'], $pathinfo['dirname'] . '/', $pathinfo['basename'], true);
+                            $url = \moodle_url::make_pluginfile_url($this->fileinfos['contextid'], $this->fileinfos['component'],
+                                PROFORMA_RESPONSE_FILE_AREA_EMBEDDED, $this->fileinfos['itemid'],
+                                $pathinfo['dirname'] . '/', $pathinfo['basename'], true);
                             $content .= "<li><a href='$url'>{$file['title']}</a></li>";
                         }
                         foreach ($files['attachedFiles'] as $file) {
                             $pathinfo = pathinfo($this->fileinfos['filepath'] . $file['filename']);
-                            $url = \moodle_url::make_pluginfile_url($this->fileinfos['contextid'], COMPONENT_NAME,
-                                PROFORMA_RESPONSE_FILE_AREA,
-                                $this->fileinfos['itemid'], $pathinfo['dirname'] . '/', $pathinfo['basename'], true);
+                            $url = \moodle_url::make_pluginfile_url($this->fileinfos['contextid'], $this->fileinfos['component'],
+                                PROFORMA_RESPONSE_FILE_AREA, $this->fileinfos['itemid'], $pathinfo['dirname'] . '/',
+                                $pathinfo['basename'], true);
                             $content .= "<li><a href='$url'>{$file['title']}</a></li>";
                         }
                         $content .= '</ul></p>';
@@ -201,16 +201,16 @@ class separate_feedback_text_renderer
                         $content .= '<p>' . get_string('files', 'qtype_moopt') . ':<br/><ul>';
                         foreach ($files['embeddedFiles'] as $file) {
                             $pathinfo = pathinfo($this->fileinfos['filepath'] . $file['id'] . '/' . $file['filename']);
-                            $url = \moodle_url::make_pluginfile_url($this->fileinfos['contextid'], COMPONENT_NAME,
-                                PROFORMA_RESPONSE_FILE_AREA_EMBEDDED,
-                                $this->fileinfos['itemid'], $pathinfo['dirname'] . '/', $pathinfo['basename'], true);
+                            $url = \moodle_url::make_pluginfile_url($this->fileinfos['contextid'], $this->fileinfos['component'],
+                                PROFORMA_RESPONSE_FILE_AREA_EMBEDDED, $this->fileinfos['itemid'],
+                                $pathinfo['dirname'] . '/', $pathinfo['basename'], true);
                             $content .= "<li><a href='$url'>{$file['title']}</a></li>";
                         }
                         foreach ($files['attachedFiles'] as $file) {
                             $pathinfo = pathinfo($this->fileinfos['filepath'] . $file['filename']);
-                            $url = \moodle_url::make_pluginfile_url($this->fileinfos['contextid'], COMPONENT_NAME,
-                                PROFORMA_RESPONSE_FILE_AREA,
-                                $this->fileinfos['itemid'], $pathinfo['dirname'] . '/', $pathinfo['basename'], true);
+                            $url = \moodle_url::make_pluginfile_url($this->fileinfos['contextid'], $this->fileinfos['component'],
+                                PROFORMA_RESPONSE_FILE_AREA, $this->fileinfos['itemid'], $pathinfo['dirname'] . '/',
+                                $pathinfo['basename'], true);
                             $content .= "<li><a href='$url'>{$file['title']}</a></li>";
                         }
                         $content .= '</ul></p>';
