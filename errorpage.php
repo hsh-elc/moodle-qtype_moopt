19df4a134fc9a11fe373c980030507adaac83cac
Paul Reiser <apaulreiser@gmail.com>
2022-05-13 19:55:22 +0200

    Make error page display different error messages

--- errorpage.php
+++ errorpage.php
@@ -3,15 +3,19 @@
 /* Setup the Page */
 require_once('../../../config.php');
 
+
 // Get params.
 $courseid = required_param('courseid', PARAM_INT);
+$error = required_param('error', PARAM_TEXT);
+$error = $error ?? 'serviceunavailable';
+$errormessage = get_string($error, 'qtype_moopt') ;
 
 // Validate them and get the corresponding objects.
 $course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
 
 $coursecontext = context_course::instance($course->id);
 
-$PAGE->set_url('/question/type/moopt/missing_capability_errorpage.php');
+$PAGE->set_url('/question/type/moopt/errorpage.php');
 
 require_login($course);
 $PAGE->set_context($coursecontext);
@@ -20,11 +24,9 @@ $PAGE->set_title(get_string('errorpage', 'qtype_moopt'));
 $PAGE->set_heading(get_string('errorpage', 'qtype_moopt'));
 $PAGE->set_pagelayout('standard');
 
-
-
 /* Output of the page's content */
 $output = $PAGE->get_renderer('qtype_moopt');
 echo $output->header();
-echo $output->render_error_msg(get_string('missingauthorcapability', 'qtype_moopt'),
-    new moodle_url('/course/view.php'), $courseid); // redirect to the startpage of the course
+echo $output->render_error_msg($errormessage,
+    new moodle_url('/course/view.php'), $courseid);
 echo $output->footer();
