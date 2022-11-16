9f2ad9ac286b650805c4ae68a8717c64ee3045d6
Paul Reiser <apaulreiser@gmail.com>
2022-11-16 17:50:12 +0100

    Update plugin version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_moopt';
-$plugin->release = '0.3.0';
-$plugin->version = 2022102500;
+$plugin->release = '0.4.0';
+$plugin->version = 2022111600;
 $plugin->requires = 2018120300;
 $plugin->maturity = MATURITY_ALPHA;
