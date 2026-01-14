9568176e1483dee166fe0e55f24ccc75f1d082fa
Robert Garmann <robert.garmann@hs-hannover.de>
2026-01-14 14:37:37 +0100

    Changed version to 0.5.0, 2026011400

--- version.php
+++ version.php
@@ -25,6 +25,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_moopt';
 $plugin->release = '0.5.0';
-$plugin->version = 2026010800;
+$plugin->version = 2026011400;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_ALPHA;
